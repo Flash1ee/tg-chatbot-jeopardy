74ef968c4a745428916bf8a17cb67ce11f633892
Volodya Larin <volodyalarin@bk.ru>
2021-02-25 20:43:23 +0300

    fix

--- run_docker_postgres.sh
+++
@@ -1,5 +0,0 @@
-sudo docker run -d --rm -P -p 127.0.0.1:5432:5432 \
-    -e POSTGRES_PASSWORD="1234" \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -v /tmp/pgdata:/var/lib/postgresql/data \
-    --name pg postgres:alpine
